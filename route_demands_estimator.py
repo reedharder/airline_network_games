dcb9f88deb6712c48e5ccfb88492b2db61704998
unknown <d29905p@T2042.kiewit.dartmouth.edu>
2015-10-08 17:17:52 -0400

    route demands creator

--- route_demands_estimator.py
+++ route_demands_estimator.py
@@ -9,4 +9,76 @@ Created on Wed Oct  7 14:02:00 2015
 functions to construct route demands and pax delay tables for arbitrary quarters and years
 '''
 
+'''
+fields needed:
+YEAR, QUARTER, NUM_FLIGHTS, ORIGIN, CONNECTION, DESTINATION, FIRST_OPERATING_CARRIER, SECOND_OPERATING_CARRIER, PASSENGERS
 
+'''
+
+import numpy as np 
+import pandas as pd
+
+#function proportion of sampled markets with less than or equal to given number of connecting flights  
+def connecting_flight_diagnostic(connections_max=2,  db1b_file='DB1B_MARKET_2007_Q1.csv',data_dir = 'C:/users/d29905p/Documents/airline_competition_paper/code/network_games/bts_data/'):
+     db1bmkts = pd.read_csv(data_dir + db1b_file)     
+     #count coupon numbers throughout database
+     coup_counts=pd.DataFrame(db1bmkts['MARKET_COUPONS'].value_counts())
+     coup_counts['mkt_count']=coup_counts[0]
+     coup_counts['index']=coup_counts.index
+     coup_counts=coup_counts.sort(columns=['index'])
+     #sum total samples and samples with number of connections>= connctions max
+     legs_sum = coup_counts.mkt_count.sum()
+     legs_under_n_sum = coup_counts[:connections_max].mkt_count.sum()
+     #return proportion
+     return  legs_under_n_sum/legs_sum
+     
+
+def create_route_demands_quarter(year =2007, quarter=1, filter_null_fares=False, filter_fare_bounds = [None, None], base_coup_fn= 'DB1B_COUPON_{year}_Q{quarter}.csv', base_mkt_fn = 'DB1B_MARKET_{year}_Q{quarter}.csv',data_dir = 'C:/users/d29905p/Documents/airline_competition_paper/code/network_games/bts_data/'):
+    #load data from DB1B markets and coupons into dataframes  
+    print('loading data...')
+    mkt_df = pd.read_csv(data_dir + base_mkt_fn.format(year=year, quarter=quarter))    
+    coup_df = pd.read_csv(data_dir + base_coup_fn.format(year=year, quarter=quarter))
+    print('filtering data...')
+    #group coupon table by mkt id
+    coup_df = coup_df.groupby('MKT_ID')
+    #filter out markets with more than one connection
+    mkt_df =mkt_df[mkt_df['MARKET_COUPONS']<=2]    
+    #optional price filtering: null/0 prices 
+    if filter_null_fares:
+         mkt_df =mkt_df[mkt_df['MARKET_FARE'].notnull() and mkt_df['MARKET_FARE'] > 0.0 ]      
+    #optional price filering: bounds
+    if filter_fare_bounds[0]: #lower bound
+         mkt_df =mkt_df[mkt_df['MARKET_FARE']>=filter_fare_bounds[0]]   
+    if filter_fare_bounds[1]: #upper bound
+         mkt_df =mkt_df[mkt_df['MARKET_FARE']<=filter_fare_bounds[1]]       
+    mkt_df = mkt_df.reset_index()
+    mkt_df['index'] =mkt_df.index
+    #function for pulling relevant market data from coupon df mkt id groups
+    #for each row of mkt 
+    print('merging data...')
+    def find_route(row,coup_df):
+        if row['index']%100000==0:
+            print( row['index'])
+        #if flight market is nonstop, simply use data DB1B markets for route demands
+        if row['MARKET_COUPONS'] ==1:
+            return pd.Series({'NUM_FLIGHTS':1,'ORIGIN':row['ORIGIN'],'CONNECTION':'' ,'DESTINATION':row['DEST'],'FIRST_OPERATING_CARRIER':row['OPERATING_CARRIER'],'SECOND_OPERATING_CARRIER':''})
+        else: #otherwise, construct connection 
+            #get market from coupons table 
+            mkt_coupons = coup_df.get_group(row['MKT_ID'])
+            #order coupons by sequence number
+            mkt_coupons = mkt_coupons.sort(columns=['SEQ_NUM'])
+            #get route data
+            origin = mkt_coupons.iloc[0]['ORIGIN']
+            connection = mkt_coupons.iloc[0]['DEST']
+            destination = mkt_coupons.iloc[1]['DEST']
+            first_op = mkt_coupons.iloc[0]['OPERATING_CARRIER']
+            second_op =mkt_coupons.iloc[1]['OPERATING_CARRIER']
+            return pd.Series({'NUM_FLIGHTS':2,'ORIGIN':origin,'CONNECTION':connection, 'DESTINATION':destination, 'FIRST_OPERATING_CARRIER':first_op,'SECOND_OPERATING_CARRIER':second_op})
+    #apply above function to each market id, to create columns listed below
+    mkt_df[['NUM_FLIGHTS','ORIGIN','CONNECTION','DESTINATION','FIRST_OPERATING_CARRIER','SECOND_OPERATING_CARRIER']] = mkt_df.apply(find_route,axis=1,coup_df=coup_df)
+    #remove coupons from memory    
+    del coup_df    
+    #aggregate same routes by same carriers     
+    print('aggregating data...')    
+    mkt_df  =  mkt_df['ORIGIN','CONNECTION','DESTINATION','FIRST_OPERATING_CARRIER','SECOND_OPERATING_CARRIER','YEAR','QUARTER','NUM_FLIGHTS','PASSENGERS','MARKET_FARE'].groupby(['ORIGIN','CONNECTION','DESTINATION','FIRST_OPERATING_CARRIER','SECOND_OPERATING_CARRIER']).aggregate({'YEAR': lambda x: int(x['YEAR'].iloc[0]),'QUARTER': lambda x: int(x['QUARTER'].iloc[0]),'NUM_FLIGHTS': lambda x: int(x['NUM_FLIGHTS'].iloc[0]),'PASSENGERS': lambda x: np.sum(x['PASSENGERS'])*10,'MARKET_FARE': lambda x: np.average(x['MARKET_FARE'], weights=x['PASSENGERS'])}).reset_index()     
+    return mkt_df
