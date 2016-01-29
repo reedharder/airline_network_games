b5fd946b043a450f2db0491c0fefef0f77ec5745
U-KIEWIT\d29905p <reedharder@gmail.com>
2016-01-29 17:03:02 -0500

    code for market analysis

---
+++ market_carrier_analysis.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 13:34:15 2016
+
+@author: d29905p
+"""
+
+#analysis of markets and market-carrier combinations
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import normalize
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition.pca import PCA
+
+
+
+
+major_carriers_2014 = ['DL','WN','UA','US','AA','B6','NK','AS','F9','VX']
+
+
+
+data_dir = "O:/documents/airline_competition_paper/code/network_games/"
+
+t100ranked_mktmod_file = data_dir + "processed_data/nonstop_competitive_markets_mktmod_ope2014.csv"
+
+major_carriers = major_carriers_2014
+
+
+
+mdata = pd.read_csv(t100ranked_mktmod_file)
+# new featues
+mdata['SEATS_PER_FLIGHT'] = mdata['SEATS']/mdata['DAILY_FREQ']
+mdata['ADDITIVE_ADJ'] = mdata['new_market']/mdata['MARKET_TOT']
+#PERHAPS ADD FEATURE FOR REGIoNAL CARRIER, ALLIANCES....NEED STATISTICAL MEASURES OF
+PCA_features = ['DISTANCE','FLIGHT_COST','MARKET_TOT','SEATS_PER_FLIGHT','ADDITIVE_ADJ','MARKET_COMPETITORS']
+PCA_features_freq = PCA_features + ['DAILY_FREQ']
+
+pca_data = mdata[mdata['UNIQUE_CARRIER'].isin(major_carriers)]
+target_carrier =  pca_data['UNIQUE_CARRIER']
+target_market = pca_data['BI_MARKET']
+target_frequency = pca_data['DAILY_FREQ']
+pca_data = pca_data[PCA_features]
+
+##pd.tools.plotting.scatter_matrix(pca_data)
+X = np.array(pca_data)
+pca = PCA(n_components=3)
+pca.fit(X)
+Y=pca.transform(normalize(X))
+
+fig = plt.figure(1, figsize=(8, 6))
+ax = Axes3D(fig, elev=-150, azim=110)
+colordict = {carrier:i for i,carrier in enumerate(major_carriers)}
+pointcolors  = [colordict[carrier] for carrier in target_carrier]
+ax.scatter(Y[:, 0], Y[:, 1], Y[:, 2], c=pointcolors)
+ax.set_title("First three PCA directions")
+ax.set_xlabel("1st eigenvector")
+ax.w_xaxis.set_ticklabels([])
+ax.set_ylabel("2nd eigenvector")
+ax.w_yaxis.set_ticklabels([])
+ax.set_zlabel("3rd eigenvector")
+ax.w_zaxis.set_ticklabels([])
