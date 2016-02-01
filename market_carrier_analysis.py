660595073aff1b3e2b604db772ae3c2bff93f01a
U-KIEWIT\d29905p <reedharder@gmail.com>
2016-02-01 18:12:41 -0500

    updated market analyis

--- market_carrier_analysis.py
+++ market_carrier_analysis.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import normalize
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition.pca import PCA
-
+import statsmodels.formula.api as sm
 
 
 
@@ -44,20 +44,38 @@ target_frequency = pca_data['DAILY_FREQ']
 pca_data = pca_data[PCA_features]
 
 ##pd.tools.plotting.scatter_matrix(pca_data)
-X = np.array(pca_data)
-pca = PCA(n_components=3)
-pca.fit(X)
-Y=pca.transform(normalize(X))
-
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
-colordict = {carrier:i for i,carrier in enumerate(major_carriers)}
-pointcolors  = [colordict[carrier] for carrier in target_carrier]
-ax.scatter(Y[:, 0], Y[:, 1], Y[:, 2], c=pointcolors)
-ax.set_title("First three PCA directions")
-ax.set_xlabel("1st eigenvector")
-ax.w_xaxis.set_ticklabels([])
-ax.set_ylabel("2nd eigenvector")
-ax.w_yaxis.set_ticklabels([])
-ax.set_zlabel("3rd eigenvector")
-ax.w_zaxis.set_ticklabels([])
+def airline_pca():
+    X = np.array(pca_data)
+    pca = PCA(n_components=3)
+    pca.fit(X)
+    Y=pca.transform(normalize(X))
+    
+    fig = plt.figure(1, figsize=(8, 6))
+    ax = Axes3D(fig, elev=-150, azim=110)
+    colordict = {carrier:i for i,carrier in enumerate(major_carriers)}
+    pointcolors  = [colordict[carrier] for carrier in target_carrier]
+    ax.scatter(Y[:, 0], Y[:, 1], Y[:, 2], c=pointcolors)
+    ax.set_title("First three PCA directions")
+    ax.set_xlabel("1st eigenvector")
+    ax.w_xaxis.set_ticklabels([])
+    ax.set_ylabel("2nd eigenvector")
+    ax.w_yaxis.set_ticklabels([])
+    ax.set_zlabel("3rd eigenvector")
+    ax.w_zaxis.set_ticklabels([])
+    
+def airline_regression():
+    #COST PER MILE??? 
+    ols_mat =pd.concat([pd.DataFrame(target_frequency),pca_data],axis=1)
+    ols_mat['FLIGHT_COST_2'] =ols_mat['FLIGHT_COST']**2
+    ols_mat['MARKET_TOT_2'] =ols_mat['MARKET_TOT']**2
+    ols_mat['COST_DEMAND'] =ols_mat['FLIGHT_COST']*ols_mat['MARKET_TOT']
+    ols_mat['DEMAND_COMPETITORS'] = ols_mat['MARKET_COMPETITORS']*ols_mat['MARKET_TOT']
+    ols_mat['COST_COMPETITORS']  = ols_mat['MARKET_COMPETITORS']*ols_mat['FLIGHT_COST']
+    fit_base = sm.ols(formula="DAILY_FREQ ~  FLIGHT_COST + FLIGHT_COST_2 + MARKET_TOT + MARKET_TOT_2 + SEATS_PER_FLIGHT +  MARKET_COMPETITORS", data = ols_mat).fit()
+    fit_base.summary()
+    fit_base = sm.ols(formula="DAILY_FREQ ~  FLIGHT_COST + FLIGHT_COST_2 + MARKET_TOT + DEMAND_COMPETITORS + MARKET_COMPETITORS", data = ols_mat).fit()
+    fit_base.summary()
+    preds =fit_base.predict()
+    MAPE = sum(abs(target_frequency-preds))/sum(target_frequency)
+   #.29....we can do better hopefully!
+    
